74c87b09cb33a9530f51c54bb4dfe73bc8fd892e
jrp8427 <70613110+jrp8427@users.noreply.github.com>
2024-02-07 21:01:07 -0500

    access website data

--- lei-organica1.py
+++ lei-organica1.py
@@ -1,9 +1,13 @@
 import pandas as pd 
 import re 
 import requests 
+from bs4 import BeautifulSoup 
 import nltk 
 
 # access the website 
+page = requests.get('https://leismunicipais.com.br/lei-organica-cordeiro-rj')
+html = page.text 
+print (html) 
 
 # import text from website into usable form 
 
